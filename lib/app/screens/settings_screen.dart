0f1a70a7320ebea9e2ae105d5b70c12ebc7332ae
lucasbennertz <lucasgcbennertz@gmail.com>
2025-08-18 21:28:50 -0300

    inicio da criação de formulario

---
+++ lib/app/screens/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:vao_estudar/app/screens/widgets/my_app_bar.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(),
+      body: Column(
+        children: [
+        
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/screens/widgets/input_text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InputText extends StatelessWidget {
+  const InputText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      
+    );
+  }
+}
